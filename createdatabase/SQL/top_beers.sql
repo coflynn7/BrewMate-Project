0da4dd047a99eb36701c5f1737a19ac8a5870965
Tyler Behle <tbehle2@gmail.com>
2025-08-15 13:00:42 -0500

    update top stored procedures

--- createdatabase/SQL/top_beers.sql
+++ createdatabase/SQL/top_beers.sql
@@ -5,12 +5,17 @@
 
 drop procedure if exists top_beers;
 delimiter $$
-create procedure top_beers(IN target_score decimal(2,1), IN offsetAmt int)
+create procedure top_beers(IN target_score decimal(2,1),IN inStyle varchar(100), IN offsetAmt int)
 begin
+if inStyle IS NULL THEN
+	set inStyle = '%';
+end if;
+
 select beer.beer_id , max(beer.Name) 'Name', avg(reviews.overall_score) 'avg'
 from reviews
-	left outer join beer
+	inner join beer
       on reviews.beer_id = beer.beer_id
+where beer.style LIKE inStyle
 group by beer.beer_id
 having avg(reviews.overall_score) >= target_score
 ORDER BY avg(reviews.overall_score) DESC
