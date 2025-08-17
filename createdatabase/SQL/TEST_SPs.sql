eb7c01201f560fd919178f2044d5c92b2c24c923
Tyler Behle <tbehle2@gmail.com>
2025-08-16 20:00:27 -0500

    add beer_fav to beer_info. add brewery_info

--- createdatabase/SQL/TEST_SPs.sql
+++ createdatabase/SQL/TEST_SPs.sql
@@ -1,5 +1,7 @@
-call top_brewery(4,0);
-call top_beers(4,50);
+select * from brewery limit 10;
+call top_brewery(4,null,null,0);
+select * from beer limit 10;
+call top_beers(4,null,50);
 call similar_beers(3);
 call user_review_diff('northyorksammy');
 call top_beer_by_brew(2);
@@ -13,13 +15,14 @@ call find_breweries('wisconsin', 'American IPA');
 call most_favorited();
 call beer_fav(12);
 call insert_beer("busch apple", "amber", 4.2, 1);
+call insert_beer("busch apple", "amber", 4.4, 1);
 select *
 from beer
 where beer_id in (select max(beer_id) from beer);
 delete from brewery where brewery_id = 30308;
 
 select count(*) from brewery;
-call insert_brewery("tylers brew 2 ", "wisconsin", "cool", "website", "n3234", 1);
+call insert_brewery("tylers brew 5 ", "wisconsin", "cool", "website", "n3234", 1);
 select count(*) from brewery;
 select * from brewery where Name like 'tylers%';
 call insert_review('northyorksammy', 12, current_timestamp(),5, 5,5, 5,5);
